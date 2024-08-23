996bde37e0084a61003a8dc0b3ecdad3d95c548f
yanyan-li <yanyan.li.camp@gmail.com>
2024-08-23 18:27:32 +0800

    first

--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,7 @@
 window.HELP_IMPROVE_VIDEOJS = false;
 
 var INTERP_BASE = "./static/interpolation/stacked";
-var NUM_INTERP_FRAMES = 11;
+var NUM_INTERP_FRAMES = 12;
 
 var interp_images = [];
 function preloadInterpolationImages() {
